0fc28049eea37ba963d3e86ba40fc17abdbd12c6
kingheedo <52102550+kingheedo@users.noreply.github.com>
2020-09-04 22:56:20 +0900

    Update CreateContent.js

--- src/CreateContent.js
+++ src/CreateContent.js
@@ -1,23 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CreateContent extends Component {
-    ChangeContent = (e) => {
-        e.preventDefault();
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {};
+  // }
+  state = {
+    todo: ""
+  };
+  ChangeContent = (e) => {
+    e.preventDefault();
+    
+  };
 
-    }
-
-    render() {
-        return (
-            <form action="/create_content" method=" post" onSubmit={this.ChangeContent}>
-                <input name="content" placeholder="오늘의 할일은?" >
-
-                </input>
-                <div>
-                    <button type="submit">작성</button>
-                </div>
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form action="/create_content" method=" post">
+        <input
+          name="content"
+          placeholder="오늘의 할일은?"
+          onChange={this.ChangeContent}
+          value={this.state.todo}
+        ></input>
+        <div>
+          <button type="submit">작성</button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default CreateContent;
